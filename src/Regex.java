0dac570e4eef7316f8be91abd8fa0dda36a14b81
michelle61219 <michelle61219@gmail.com>
2017-09-06 14:40:43 -0600

    changed the comment for email

--- src/Regex.java
+++ src/Regex.java
@@ -35,7 +35,7 @@ public class Regex {
 		
 		/*
 		 * Search for the email address
-		 * johnsmith@gmail.com
+		 * johnsmith@hotmail.com
 		 */	
 		 	regexChecker("[A-Za-z0-9._%-]+@[A-Za-z0-9._-]+\\.[A-Za-z]{2,4}", longString);
 		
